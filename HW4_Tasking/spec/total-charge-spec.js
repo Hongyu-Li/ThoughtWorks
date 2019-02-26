dcd7d67e571190ca3ad2a13c9d6e2889bc45a9c7
Hongyu-Li <hl3099@columbia.edu>
2019-02-25 22:52:16 -0500

    get items charge based on order information

--- HW4_Tasking/spec/total-charge-spec.js
+++ HW4_Tasking/spec/total-charge-spec.js
@@ -1,13 +1,13 @@
 var expect = require('chai').expect;
 var loadAllItems = require('../src/items.js')
-var getTotalCharge = require('../src/total-charge.js')
+var getItemsCharge = require('../src/best-charge.js')
 
-describe('calculate total charge', function() {
+describe('calculate items charge', function() {
 
   it("returns total charge w/o promotions", function() {
     let OrderInfo = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
-    let ItemsInfo = loadAllItems();
-    var ItemsChargeSheet, TotalCharge = getTotalCharge(OrderInfo, ItemsInfo);
+    var ItemsInfo = loadAllItems();
+    var ItemsChargeSheet = getItemsCharge(OrderInfo, ItemsInfo);
     var sheet_expect_result = [{id: 'ITEM0001', name: '黄焖鸡', count: 1, itemprice: 18},
                                {id: 'ITEM0013', name: '肉夹馍', count: 2, itemprice: 12},
                                {id: 'ITEM0022', name: '凉皮', count: 1, itemprice: 8}];
--- HW4_Tasking/src/total-charge.js
+++
@@ -1,8 +0,0 @@
-function getTotalCharge(OrderInfo, ItemsInfo){
-  let ItemsChargeSheet = [{id: 'ITEM0001', name: '黄焖鸡', count: 1, itemprice: 18},
-                          {id: 'ITEM0013', name: '肉夹馍', count: 2, itemprice: 12},
-                          {id: 'ITEM0022', name: '凉皮', count: 1, itemprice: 8}];
-  return ItemsChargeSheet;
-};
-
-module.exports = getTotalCharge;
