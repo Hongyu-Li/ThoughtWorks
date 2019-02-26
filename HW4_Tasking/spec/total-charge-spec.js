ccb96c06d78a0ee56dcb8df05cda56fb36d32180
Hongyu-Li <hl3099@columbia.edu>
2019-02-25 23:09:41 -0500

    task 1-1: get items charge based on order array.

--- HW4_Tasking/spec/total-charge-spec.js
+++ HW4_Tasking/spec/total-charge-spec.js
@@ -1,10 +1,10 @@
 var expect = require('chai').expect;
 var loadAllItems = require('../src/items.js')
-var getItemsCharge = require('../src/best-charge.js')
+var getItemsCharge = require('../src/items-charge.js')
 
 describe('calculate items charge', function() {
 
-  it("returns total charge w/o promotions", function() {
+  it("returns items charge w/o promotions", function() {
     let OrderInfo = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
     var ItemsInfo = loadAllItems();
     var ItemsChargeSheet = getItemsCharge(OrderInfo, ItemsInfo);
@@ -13,4 +13,5 @@ describe('calculate items charge', function() {
                                {id: 'ITEM0022', name: '凉皮', count: 1, itemprice: 8}];
     expect(sheet_expect_result).to.eql(ItemsChargeSheet);
   });
+
 });
---
+++ HW4_Tasking/src/items-charge.js
@@ -0,0 +1,19 @@
+// Task 1
+function getItemsCharge(OrderInfo, ItemsInfo){
+    var ItemsChargeSheet = []
+    for (var i=0; i < OrderInfo.length; i++){
+      order = OrderInfo[i].split('x');
+      id =  order[0].trim();
+      count = Number(order[1].trim());
+      id_array = ItemsInfo.map(function(obj){return obj['id']});
+      item_info = ItemsInfo[id_array.indexOf(id)];
+      record = {id: id,
+                name: item_info['name'],
+                count: count,
+                itemprice:  item_info['price']*count};
+      ItemsChargeSheet.push(record);
+    };
+    return ItemsChargeSheet;
+};
+
+module.exports = getItemsCharge; 
